7c0ac03d4849c9ea72816daac025043ed2499a33
amaljose-aj <57029243+amaljose-aj@users.noreply.github.com>
2025-08-01 10:38:09 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <assert.h>
 #include <vector> 
-#include <string> 
+#include <string>
 
 struct ColorPair {
     int code;
@@ -13,8 +13,9 @@ std::vector<ColorPair> generateColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     std::vector<ColorPair> colorMap;
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
+    int i = 0, j = 0;
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j < 5; j++) {
             colorMap.push_back({i * 5 + j, majorColor[i], minorColor[j]});
         }
     }
@@ -28,9 +29,4 @@ void testPrintColorMap() {
     assert(result[0].majorColor == "White" && result[0].minorColor == "Blue");
     assert(result[24].majorColor == "Violet" && result[24].minorColor == "Slate");
     std::cout << "All is well (maybe!)\n";
-}
-
-int main() {
-    testPrintColorMap();
-    return 0;
 }
